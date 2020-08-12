b9bf954a2c375568a1ffc0c03366be62d71715c1
Eric Wood <epwood1@gmail.com>
2020-08-12 16:52:48 -0700

    sliding window and eating cookies

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,47 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    max_arr = [] #array of maximums
+    cur_window = []#holds current window value
 
-    pass
+    #first window - it doesn't behave like the others
+    max_num = nums[0]
+    max_index = 0
+    index = 1
+
+    while index < k: 
+        if nums[index] > max_num:
+            max_num = nums[index]
+            max_index = index
+
+        index += 1
+
+    max_arr.append(max_num)
+    #end first window
+
+    first_in_window = 1
+    #2nd window and greater
+    while index < len(nums):
+        #print("cur_window " + str(cur_window))
+        if max_index < first_in_window:
+            max_num = nums[first_in_window]
+            max_index = first_in_window
+            
+            for i in range(first_in_window + 1, first_in_window + k):
+                if(nums[i] >= max_num):
+                    max_num = nums[i]
+                    max_index = i
+        elif nums[index] >= max_num:
+            max_num = nums[index]
+            max_index = index
+
+
+        index += 1
+        first_in_window += 1
+        max_arr.append(max_num)       
+    #end windows2+
+
+    return max_arr
 
 
 if __name__ == '__main__':
